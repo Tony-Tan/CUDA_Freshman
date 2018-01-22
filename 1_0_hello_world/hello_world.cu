bc3f4576006f9461f6bbfefcf31715fefbfe8358
Tony-Tan <363806348@qq.com>
2018-01-22 20:51:04 +0800

    1_0_hello_world

---
+++ 1_0_hello_world/hello_world.cu
@@ -0,0 +1,12 @@
+#include<stdio.h>
+__global__ void hello_world(void)
+{
+  printf("GPU: Hello world!\n");
+}
+int main()
+{
+  printf("CPU: Hello world!\n");
+  hello_world<<<1,10>>>();
+  cudaDeviceReset();//if no this line ,it can not output hello world from gpu
+  return 0;
+}
