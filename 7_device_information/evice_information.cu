31d15de58ab8747dfedc425bb7f3f98279a26ad9
Tony-Tan <363806348@qq.com>
2018-03-10 23:02:41 +0800

    device information

---
+++ 7_device_information/evice_information.cu
@@ -0,0 +1,71 @@
+#include <cuda_runtime.h>
+#include <stdio.h>
+
+int main(int argc,char** argv)
+{
+    printf("%s Starting ...\n",argv[0])
+    int deviceCount = 0;
+    cudaError_t error_id = cudaGetDeviceCount(&deviceCount);
+    if(error_id!=cudaSuccess)
+    {
+        printf("cudaGetDeviceCount returned %d\n ->%s\n",
+              (int)error_id,cudaGetErrorString(error_id));
+        printf("Result = FAIL\n");
+        exit(EXIT_FAILURE);
+    }
+    if(deviceCount==0)
+    {
+        printf("There are no available device(s) that support CUDA\n");
+    }
+    else
+    {
+        printf("Detected %d CUDA Capable device(s)\n",deviceCount);
+    }
+    int dev=0,driverVersion=0,runtimeVersion=0;
+    cudaSetDevice(dev);
+    cudaDeviceProp deviceProp;
+    cudaGetDeviceProperties(&deviceProp,dev);
+    printf("Device %d:\"%s\"\n",dev,deviceProp.name);
+    cudaDriverGetVersion(&deviceVersion);
+    cudaRuntimeGetVersion(&runtimeVersion);
+    printf("  CUDA Driver Version / Runtime Version         %d.%d  /  %d.%d\n",
+        driverVersion/1000,(driverVersion%100)/10,
+        runtimeVersion/1000,(runtimeVersion%100)/10);
+    printf("  CUDA Capability Major/Minor version number:   %d.%d\n",
+        deviceProp.major,deviceProp.minor);
+    printf("  Total amount of global memory:                %.2f MBytes (%llu bytes\n)",
+            (float)deviceProp.totalGlobalMem/pow(1024.0,3));
+    printf("  GPU Clock rate:                               %.0f MHz (%0.2f GHz)\n",
+            deviceProp.clockRate*1e-3f,deviceProp.clockRate*1e-6f);
+    printf("  Memory Bus width:                             %d-bits\n",
+            deviceProp.memoryBusWidth);
+    if (deviceProp.l2CacheSize)
+    {
+        printf("  L2 Cache Size:                            %d bytes\n",
+                deviceProp.l2CacheSize);
+    }
+    printf("  Max Texture Dimension Size (x,y,z)            1D=(%d),2D=(%d,%d),3D=(%d,%d,%d)\n",
+            deviceProp.maxTexture1D,deviceProp.maxTexture2D[0],deviceProp.maxTexture2D[1]
+            ,deviceProp.maxTexture3D[0],deviceProp.maxTexture3D[1],deviceProp.maxTexture3D[2]);
+    printf("  Max Layered Texture Size (dim) x layers       1D=(%d) x %d,2D=(%d,%d) x %d\n",
+            deviceProp.maxTexture1DLayered[0],deviceProp.maxTexture1DLayered[1],
+            deviceProp.maxTexture2DLayered[0],deviceProp.maxTexture2DLayered[1],
+            deviceProp.maxTexture2DLayered[2]);
+    printf("  Total amount of constant memory               %lu bytes\n",
+            deviceProp.totalConstMem);
+    printf("  Total amount of shared memory per block:      %lu bytes\n",
+            deviceProp.sharedMemPerBlock);
+    printf("  Total number of registers available per block:%d\n",
+            deviceProp.regsPerBlock);
+    printf("  Wrap size:                                    %d\n",deviceProp.warpSize);
+    printf("  Maximun number of thread per multiprocesser:  %d\n",
+            deviceProp.maxThreadsPerMultiProcessor);
+    printf("  Maximun number of thread per block:           %d\n",
+            deviceProp.maxThreadsPerBlock);
+    printf("  Maximun size of each dimension of a block:    %d x %d x %d\n",
+            deviceProp.maxThreadsDim[0],deviceProp.maxThreadsDim[1],deviceProp.maxThreadsDim[2]);
+    printf("  Maximun size of each dimension of a grid:     %d x %d x %d\n",
+            deviceProp.maxGridDim[0],deviceProp.maxGridDim[1],deviceProp.maxGridDim[2]);
+    printf("  Maximu memory pitch                           %lu bytes\n",deviceProp.memPitch);
+    exit(EXIT_SUCCESS);
+}
