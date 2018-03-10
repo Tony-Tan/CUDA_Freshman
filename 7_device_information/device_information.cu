383308f17be643b40e6849a6ecc7abf54550ced1
Tony-Tan <363806348@qq.com>
2018-03-10 23:29:13 +0800

    device debut

--- 7_device_information/device_information.cu
+++ 7_device_information/device_information.cu
@@ -3,7 +3,7 @@
 
 int main(int argc,char** argv)
 {
-    printf("%s Starting ...\n",argv[0])
+    printf("%s Starting ...\n",argv[0]);
     int deviceCount = 0;
     cudaError_t error_id = cudaGetDeviceCount(&deviceCount);
     if(error_id!=cudaSuccess)
@@ -26,14 +26,14 @@ int main(int argc,char** argv)
     cudaDeviceProp deviceProp;
     cudaGetDeviceProperties(&deviceProp,dev);
     printf("Device %d:\"%s\"\n",dev,deviceProp.name);
-    cudaDriverGetVersion(&deviceVersion);
+    cudaDriverGetVersion(&driverVersion);
     cudaRuntimeGetVersion(&runtimeVersion);
     printf("  CUDA Driver Version / Runtime Version         %d.%d  /  %d.%d\n",
         driverVersion/1000,(driverVersion%100)/10,
         runtimeVersion/1000,(runtimeVersion%100)/10);
     printf("  CUDA Capability Major/Minor version number:   %d.%d\n",
         deviceProp.major,deviceProp.minor);
-    printf("  Total amount of global memory:                %.2f MBytes (%llu bytes\n)",
+    printf("  Total amount of global memory:                %.2f MBytes (%llu bytes)\n",
             (float)deviceProp.totalGlobalMem/pow(1024.0,3));
     printf("  GPU Clock rate:                               %.0f MHz (%0.2f GHz)\n",
             deviceProp.clockRate*1e-3f,deviceProp.clockRate*1e-6f);
@@ -41,7 +41,7 @@ int main(int argc,char** argv)
             deviceProp.memoryBusWidth);
     if (deviceProp.l2CacheSize)
     {
-        printf("  L2 Cache Size:                            %d bytes\n",
+        printf("  L2 Cache Size:                            	%d bytes\n",
                 deviceProp.l2CacheSize);
     }
     printf("  Max Texture Dimension Size (x,y,z)            1D=(%d),2D=(%d,%d),3D=(%d,%d,%d)\n",
@@ -65,7 +65,9 @@ int main(int argc,char** argv)
     printf("  Maximun size of each dimension of a block:    %d x %d x %d\n",
             deviceProp.maxThreadsDim[0],deviceProp.maxThreadsDim[1],deviceProp.maxThreadsDim[2]);
     printf("  Maximun size of each dimension of a grid:     %d x %d x %d\n",
-            deviceProp.maxGridDim[0],deviceProp.maxGridDim[1],deviceProp.maxGridDim[2]);
+            deviceProp.maxGridSize[0],
+	    deviceProp.maxGridSize[1],
+	    deviceProp.maxGridSize[2]);
     printf("  Maximu memory pitch                           %lu bytes\n",deviceProp.memPitch);
     exit(EXIT_SUCCESS);
 }
